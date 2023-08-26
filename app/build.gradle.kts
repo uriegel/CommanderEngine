96d34beda738a6441fd0cb33e19690454b199910
Uwe Riegel <uriegel@hotmail.de>
2023-08-26 15:20:54 +0200

    Converted to gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,92 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "de.uriegel.commanderengine"
+    compileSdk = 33
+
+    signingConfigs {
+        create("signing") {
+            storeFile = file("/home/uwe/Dokumente/Entwicklung/AndroidKeyStore/keystore.jks")
+            storePassword = extra["ANDROID_STORE_PASSWORD"].toString()
+            keyAlias = "androidKey"
+            keyPassword = extra["ANDROID_KEY_PASSWORD"].toString()
+        }
+    }
+
+    defaultConfig {
+        applicationId = "de.uriegel.commanderengine"
+        minSdk = 22
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("signing")
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("signing")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packagingOptions {
+        jniLibs {
+            excludes += "META-INF/*"
+        }
+        resources {
+            excludes += "/META-INF/*"
+        }
+    }
+}
+
+kotlin {
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.RequiresOptIn")
+        }
+    }
+}
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.ui:ui:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation("androidx.compose.material:material:1.3.1")
+    implementation("com.google.accompanist:accompanist-permissions:0.28.0")
+    implementation("io.ktor:ktor:2.1.2")
+    implementation("io.ktor:ktor-serialization-gson:2.1.2")
+    implementation("io.ktor:ktor-server-netty:2.1.2")
+    implementation("io.ktor:ktor-server-content-negotiation:2.1.2")
+    implementation("io.ktor:ktor-server-cors:2.1.2")
+    implementation("io.ktor:ktor-server-auto-head-response:2.1.2")
+    implementation("io.ktor:ktor-server-partial-content:2.1.2")
+}
