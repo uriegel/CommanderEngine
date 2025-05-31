48f7e1f991aab0ef4cea6e35e8131be1fd1d812b
uriegel <uriegel@hotmail.de>
2025-05-31 11:27:35 +0200

    Modern theme adaptions

--- app/src/main/java/de/uriegel/commanderengine/ui/MainActivity.kt
+++ app/src/main/java/de/uriegel/commanderengine/ui/MainActivity.kt
@@ -6,9 +6,9 @@ import android.os.Bundle
 import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             CommanderEngineTheme {
                 var permissionState by remember {
